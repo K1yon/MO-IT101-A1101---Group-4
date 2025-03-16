b6f54853b6b8426447b06beefc0e29fccadbc144
K1yon <keonclarkfdeleon@gmail.com>
2025-03-17 04:34:49 +0800

    Delete MotorPHPayrollSystem directory

--- MotorPHPayrollSystem/src/main/java/utils/PhilHealthCalculator.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class PhilHealthCalculator {
-    public static double getPhilHealthContribution(double salary) {
-        double contribution = (salary * 0.03) / 2; // 3% contribution, employee share is half
-        return contribution; // Return the employee share
-    }
-}
